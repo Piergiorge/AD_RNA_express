5eb08f992e126dc8e3cab43cfc2b1c5c7363912c
Rafael M Piergiorge <rafaelpiergiorge@gmail.com>
2023-06-22 15:41:28 -0300

    Create series_matrix2pdata.sh

---
+++ array/series_matrix2pdata.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# series_matrix to pdata
+
+FILE="$1"
+sed -n '/!Sample_title/{:loop;p;/!Sample_data_row_count/q;N;s/.*\n//;b loop};' "$FILE" > temp
+# row 2 columns
+awk -F "\t" '{ for (i=1; i<=NF; i++) RtoC[i]= (RtoC[i]!=""? RtoC[i] FS $i: $i) }    END{ for (i in RtoC) print RtoC[i] }' temp | sed -E 's/\!//g' > pdata.txt
